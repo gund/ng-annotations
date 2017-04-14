b9c13487cef01a7da4b7745f16e332ec4a548012
gund <malkevich.alex@gmail.com>
2017-04-15 01:17:26 +0400

    fix(release): Fix semantic releasing

--- scripts/release.js
+++ scripts/release.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const copyDir = require('copy-dir');
 const package = require('../package.json');
 
 delete package.devDependencies;
@@ -6,3 +7,6 @@ delete package.config;
 delete package.scripts;
 
 fs.writeFileSync('dist/package.json', JSON.stringify(package, null, '  '));
+
+// Need .git in publishing folder
+copyDir.sync('.git', 'dist/.git');
