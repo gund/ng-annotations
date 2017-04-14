03b455fef56650885565966bff6a1dd1e0f8f3ce
gund <malkevich.alex@gmail.com>
2017-04-15 01:39:32 +0400

    fix(release): Add readme and licence to npm package

--- scripts/release.js
+++ scripts/release.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const copyDir = require('copy-dir');
+const copyfiles = require('copyfiles');
 const package = require('../package.json');
 
 delete package.devDependencies;
@@ -7,6 +8,16 @@ delete package.config;
 delete package.scripts;
 
 fs.writeFileSync('dist/package.json', JSON.stringify(package, null, '  '));
+console.log('package.json written to dist');
+
+copyfiles([
+  'README.MD',
+  'LICENSE',
+  'yarn.lock',
+  'dist' // Destinantion
+], {}, () => null);
+console.log('Other files copied to dist');
 
 // Need .git in publishing folder
 copyDir.sync('.git', 'dist/.git');
+console.log('.git folder copied to dist');
